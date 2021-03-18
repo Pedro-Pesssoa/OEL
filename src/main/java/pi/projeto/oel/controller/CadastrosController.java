ab377e096a443c96e269d4dfbfa345d3da80ee7b
Pedro-Pesssoa <63820078+Pedro-Pesssoa@users.noreply.github.com>
2021-03-18 17:59:35 -0300

    alguns testes

--- src/main/java/pi/projeto/oel/controller/CadastrosController.java
+++ src/main/java/pi/projeto/oel/controller/CadastrosController.java
@@ -2,6 +2,8 @@ package pi.projeto.oel.controller;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -25,7 +27,7 @@ public class CadastrosController {
 	private UsuarioRepository ur;
 	
 	@GetMapping("/usuario")
-	public String cadastrarUsuario (Usuario usuario) {
+	public String cadastrarUsuario (@Valid Usuario usuario) {
 		
 		System.out.println(usuario);
 		ur.save(usuario);
--- src/main/java/pi/projeto/oel/models/Usuario.java
+++ src/main/java/pi/projeto/oel/models/Usuario.java
@@ -1,13 +1,9 @@
 package pi.projeto.oel.models;
 
-import java.time.LocalDate;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-
-import org.springframework.format.annotation.DateTimeFormat;
 
 @Entity
 public class Usuario {
@@ -16,10 +12,11 @@ public class Usuario {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
 
+	
 	private String nome;
 
-	@DateTimeFormat(pattern = "dd-MM-yyyy")
-	private LocalDate dataNasc;
+	
+	private String dataNasc;
 	
 	
 	private String cidade;
@@ -47,11 +44,11 @@ public class Usuario {
 		this.nome = nome;
 	}
 
-	public LocalDate getIdade() {
+	public String getIdade() {
 		return dataNasc;
 	}
 
-	public void setIdade(LocalDate idade) {
+	public void setIdade(String idade) {
 		this.dataNasc = idade;
 	}
 
