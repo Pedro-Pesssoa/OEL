1b0b593d1af9b2629b998eed229f983408d156cd
Laura Oliveira <laurapaulo2017@gmail.com>
2021-03-18 20:50:06 -0300

    criando paginas de detalhar lixeira

--- src/main/java/pi/projeto/oel/controller/CadastrosController.java
+++ src/main/java/pi/projeto/oel/controller/CadastrosController.java
@@ -1,10 +1,12 @@
 package pi.projeto.oel.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -16,37 +18,54 @@ import pi.projeto.oel.repositories.UsuarioRepository;
 @Controller
 @RequestMapping("/oel")
 public class CadastrosController {
-	
-	
+
 	@Autowired
 	private LixeiraRepository lr;
-	
+
 	@Autowired
 	private UsuarioRepository ur;
-	
+
 	@GetMapping("/usuario")
-	public String cadastrarUsuario (Usuario usuario) {
-		
+	public String cadastrarUsuario(Usuario usuario) {
+
 		System.out.println(usuario);
 		ur.save(usuario);
-		
+
 		return "cadastroUsuario";
-		
+
 	}
+
 	@GetMapping("/lixeira")
-	public String cadastrarLixeira (Lixeira lixeira) {
-		
+	public String cadastrarLixeira(Lixeira lixeira) {
+
 		System.out.println(lixeira);
 		lr.save(lixeira);
-		
+
 		return "cadastroLixeira";
 	}
-	
+
 	@GetMapping
 	public ModelAndView listar() {
 		List<Lixeira> lixeiras = lr.findAll();
 		ModelAndView mv = new ModelAndView("listLixeiras");
 		mv.addObject("lixeiras", lixeiras);
 		return mv;
+	}
+
+	@GetMapping("/{id}")
+	public ModelAndView detalhar(@PathVariable Long id) {
+		ModelAndView md = new ModelAndView();
+		Optional<Lixeira> opt = lr.findById(id);
+
+		if (opt.isEmpty()) {
+			md.setViewName("redirect:/oel");
+			return md;
+		}
+
+		md.setViewName("/detalhes");
+		Lixeira lixeira = opt.get();
+		md.addObject("lixeira", lixeira);
+
+		return md;
 	}
 }
