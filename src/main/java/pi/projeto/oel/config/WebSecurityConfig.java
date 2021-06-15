b31f0eee97f2a4a2ba859a73ea15945b9daae524
Pedro-Pesssoa <63820078+Pedro-Pesssoa@users.noreply.github.com>
2021-06-15 14:36:30 -0300

    logout)

--- src/main/java/pi/projeto/oel/config/WebSecurityConfig.java
+++ src/main/java/pi/projeto/oel/config/WebSecurityConfig.java
@@ -3,6 +3,7 @@ package pi.projeto.oel.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
@@ -20,6 +21,13 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 		.formLogin()
 			.loginPage("/oel/login")
 			.defaultSuccessUrl("/oel", true)
+			.permitAll()
+		.and()
+		.logout()
+			.logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+			.logoutSuccessUrl("/oel")
 			.permitAll();
+		
+		
 	}
 }
